3323ee4a8b3bc1c53775a135b783f89d77fb3078
ShadowQueen06 <malk.lol140@gmail.com>
2026-07-08 03:00:31 +0200

    config.js

---
+++ config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  serverName: "العراق العظيم",
+
+  categories: {
+    logs: "1524212997118230750"
+  },
+
+  channels: {
+    messageLogs: "1524213080953983066",
+    voiceLogs: "1524213537986187385",
+    memberLogs: "1524215072887406663",
+    modLogs: "1524215193314529340",
+    ticketLogs: "1524215309031178260"
+  },
+
+  colors: {
+    success: "#57F287",
+    warning: "#FEE75C",
+    danger: "#ED4245",
+    info: "#5865F2",
+    gold: "#C8A24A"
+  }
+};
