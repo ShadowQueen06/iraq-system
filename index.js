117f3d2433c073b8cef64c13d7574ff5f2a21d66
ShadowQueen06 <malk.lol140@gmail.com>
2026-07-08 03:09:22 +0200

    index.js

--- index.js
+++ index.js
@@ -14,11 +14,16 @@ const client = new Client({
   ]
 });
 
+// Events
+const messageDelete = require("./events/messageDelete");
+
 client.once("ready", () => {
   console.log("==================================");
   console.log(`${client.user.tag} is online.`);
   console.log("IRAQ SYSTEM is ready.");
   console.log("==================================");
 });
+
+client.on("messageDelete", messageDelete);
 
 client.login(process.env.TOKEN);
